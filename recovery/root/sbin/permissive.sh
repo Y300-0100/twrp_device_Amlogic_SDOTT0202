843253d644c7b8d460ea4ca6f943875058743607
Y300-0100 <bosko.brajovich@gmail.com>
2022-01-14 20:12:48 +0100

    Update permissive.sh
    Oups.

--- recovery/root/sbin/permissive.sh
+++ recovery/root/sbin/permissive.sh
@@ -4,5 +4,5 @@ setenforce 0
 
 # Get your device's block path where "system", "recovery", etc. lives.
 # That can be "/dev/block/bootdevice/by-name" or something like that.
-mkdir -p /dev/block/platform/mtk-msdc.0/by-name/
+mkdir -p /dev/block/platform/ffe07000.emmc/by-name/
 busybox mount -o bind /dev/block/platform/ffe07000.emmc/by-name/ /dev/block/platform/ffe07000.emmc/by-name/
